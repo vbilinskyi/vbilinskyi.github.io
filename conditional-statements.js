14b495886e8910fe9b7b0b533388a1922222654b
Vitalii Bilinskyi <bilinskyivitalii@gmail.com>
2020-06-15 18:26:50 +0300

    Условные выражения и циклы

---
+++ conditional-statements.js
@@ -0,0 +1,35 @@
+let isMonday = true;
+
+if (isMonday) {
+    console.log('Yes it is Monday now');
+} else {
+    console.log('No it is Wednesday now');
+}
+
+
+let dayInMay = 31;
+let dayInJune = 30;
+
+if (dayInMay > dayInJune) {
+    console.log('May is longer then June');
+} else {
+    console.log('May is no longer then June');
+}
+
+let num1 = 15;
+let num2 = 15;
+let str1 = '15';
+
+if (num1 >= num2) {
+    console.log('Variant 1');
+} else {
+    console.log('Variant 2');
+}
+
+if (num1 == str1) {
+    console.log('num1 and str1 pretty the same');   
+}
+
+
+
+
---
+++ loops.js
@@ -0,0 +1,10 @@
+let num = 1;
+let i = 0;
+
+for (i; num >= 10; i++) {
+    num = num + 1;
+}
+
+console.log(num);
+
+
